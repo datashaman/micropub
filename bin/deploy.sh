cf370643318aeb94cfcc319167e55b15ca521b52
datashaman <marlinf@datashaman.com>
2020-01-11 15:33:41 +0200

    pull in composer deps

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,6 +12,9 @@ ssh $DEPLOY_HOST "
   echo 'Pull latest changes'
   git pull
 
+  echo 'Install composer dependencies'
+  composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --no-suggest --optimize-autoloader
+
   echo 'Install npm dependencies'
   npm install
 
