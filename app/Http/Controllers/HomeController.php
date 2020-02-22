9f88bb704dee563e8c2f5dcb89d6ca2d5cb45ec8
datashaman <marlinf@datashaman.com>
2020-02-22 18:46:00 +0200

    try this

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use GrahamCampbell\GitHub\Facades\GitHubFactory;
+use GrahamCampbell\GitHub\GitHubFactory;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
