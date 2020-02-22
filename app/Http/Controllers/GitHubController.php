4f712780354d8134d7b31b50274d598b8a9daf5a
datashaman <marlinf@datashaman.com>
2020-02-22 15:10:40 +0200

    wip

--- app/Http/Controllers/GitHubController.php
+++ app/Http/Controllers/GitHubController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Socialite;
 
-class LoginController extends Controller
+class GitHubController extends Controller
 {
     public function redirect()
     {
