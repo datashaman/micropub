df8fa1dd55a9e968fa658e69e8a98c6cdae4fdae
datashaman <marlinf@datashaman.com>
2020-02-22 18:22:42 +0200

    try this

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,13 @@
 <div>
-    Logged in <a href="https://indieauth.com">IndieAuth</a> as <a href="{{ Session::get('user.me') }}">{{ Session::get('user.me') }}</a>  with scope(s) <em>{{ Session::get('user.scope') }}</em>.
+    Logged in to <a href="https://indieauth.com" target="_blank" rel="noopener noreferrer">IndieAuth</a>
+    as <a href="{{ Session::get('user.me') }}">{{ Session::get('user.me') }}</a>  with scope(s) <em>{{ Session::get('user.scope') }}</em>.
     <a href="{{ route('indieauth.logout') }}">Logout</a>
 </div>
 
 @auth
     <div>
-        Logged in to <a href="https://github.com">GitHub</a> as <a href="https://github.com/{{ auth()->user()->name }}">{{ auth()->user()->name }}</a>.
+        Logged in to <a href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a>
+        as <a href="https://github.com/{{ auth()->user()->name }}" target="_blank" rel="noopener noreferrer">{{ auth()->user()->name }}</a>.
         <a href="{{ route('github.logout') }}">Logout</a>
     </div>
 @endauth
