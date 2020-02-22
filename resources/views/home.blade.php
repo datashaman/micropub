a6cf21de5e164fa321a0e23e7a45c924399ac65b
datashaman <marlinf@datashaman.com>
2020-02-22 15:44:58 +0200

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
+<div>
 Logged in as <a href="{{ Session::get('user.me') }}">{{ Session::get('user.me') }}</a>  with scope(s) <em>{{ Session::get('user.scope') }}</em>.
-<a href="{{ route('auth.logout') }}">Logout</a>
+<a href="{{ route('indieauth.logout') }}">Logout</a>
+</div>
 
-<a href="{{ route('github.redirect') }}">Link GitHub</a>
+<div>
+    <a href="{{ route('github.redirect') }}">Link GitHub</a>
+</div>
