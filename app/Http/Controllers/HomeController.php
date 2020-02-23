d883f6d9d5b1294d16870cf7455918935bab12c9
datashaman <marlinf@datashaman.com>
2020-02-23 07:29:25 +0200

    wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use DOMDocument;
 use GrahamCampbell\GitHub\GitHubFactory;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Socialite;
+use Symfony\Component\DomCrawler\Crawler;
 
 class HomeController extends Controller
 {
@@ -13,15 +15,18 @@ class HomeController extends Controller
     {
         $me = session('user.me');
 
-        $client = new Client();
-        $response = $client->get($me);
-
-        dd(
+        $client = new Client(
             [
-                'headers' => $response->getHeaders(),
-                'body' => $response->getBody(),
+                'connect_timeout' => 2.0,
+                'timeout' => 4.0,
             ]
         );
+
+        $response = $client->get($me);
+        $crawler = new Crawler((string) $response->getBody());
+        $links = $crawler->filter('head link[rel]');
+
+        dd($links);
 
         return view('home');
     }
