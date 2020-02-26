65f96c3f262c0c389182cdf15130ec07b7b23fcb
datashaman <marlinf@datashaman.com>
2020-02-26 08:22:23 +0200

    wip

--- resources/views/types/entry.blade.php
+++ resources/views/types/entry.blade.php
@@ -1,8 +1,3 @@
 ---
 {!! yaml_dump($source, 10) !!}
 ---
-@if($contentType === 'html')
-{!! Arr::get($source, 'content.html') !!}
-@elseif($contentType === 'text')
-{{ Arr::get($source, 'content') }}
-@endif
